5cf91acbc13485c7101370a0a6ee2aa4ad24d22e
Jhersonvasco <jjvasco9@misena.edu.co>
2016-05-19 17:44:54 -0500

    ultimos cambios 19/05/16

---
+++ Website/html/SuperAdmin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Administrador</title>
+	<meta name="Description" content="">
+	<meta name="viewport" content="width=400, initial-scale=1.0">
+	<!-- Compiled and minified CSS -->
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+  	<link rel="stylesheet" href="../css/style.css">
+  	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,100italic,700,900,900italic,700italic,500italic,500,400italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+  	<link href='https://fonts.googleapis.com/css?family=Montserrat:700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+</head>
+<body>
+	<?php include('menus/menuSadmin.php'); ?>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<!-- Compiled and minified JavaScript -->
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+
+	<script type="text/javascript">
+		$(document).ready(function(){
+     		$('.slider').slider({full_width: true});
+ 		});
+
+
+ 		$('#textarea1').trigger('autoresize');
+
+ 		
+ 		$(document).ready(function() {
+    		$('input#input_text, textarea#textarea1').characterCounter();
+  		});
+
+  		$(document).ready(function(){
+  			var altura = $('.menu').offset().top;
+  			$(window).on('scroll',function(){
+  				if($(window).scrollTop()>altura){
+  					$('.menu').addClass('menu-fixed');
+  				}else{
+  					$('.menu').removeClass('menu-fixed');
+						
+  				}
+  			})
+  		});
+  		$(".button-collapse").sideNav();
+	</script>
+</body>
+</html>
---
+++ Website/html/menus/menuSadmin.php
@@ -0,0 +1,28 @@
+<nav class="z-depth-2 m12 s12 menu">
+	<div class="nav-wrapper orange z-depth-1">
+		<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons fa fa-bars"></i></a>
+		<a href="#slider2" class="black-text right" style="margin-right: 20px;"><i class="fa fa-power-off fa-3x"></i></a>
+		<ul class="hide-on-med-and-down">
+			<li><a href="#cabeza" class="black-text">Inicio</a></li>
+			<li><a href=".slider" class="black-text">Ingresos Al Gimnasio</a></li>
+			<li><a href="#quienes_somos" class="black-text">Registrar Cliente</a></li>
+			<li><a href="#slider2" class="black-text">Clientes En Mora</a></li>
+			<li><a href="#slider2" class="black-text">Citas Medicas</a></li>
+			<li><a href="#slider2" class="black-text">Usuarios</a></li>
+			<li><a href="#slider2" class="black-text">Valoraciones</a></li>
+			<li><a href="#slider2" class="black-text">Roles</a></li>
+			<li><a href="#slider2" class="black-text">Planes</a></li>
+			<li><a href="#slider2" class="black-text">Recursos Fisicos</a></li>
+
+
+		</ul>
+		<ul class="side-nav" id="mobile-demo">
+			<li><a href="#cabeza" class="black-text"><i class="fa fa-home"></i>Inicio</a></li>
+			<li><a href=".slider" class="black-text"><i class="fa fa-sign-in"></i>Ingresos</a></li>
+			<li><a href="#quienes_somos" class="black-text"><i class="fa fa-user-plus"></i>Nuevo Cliente</a></li>
+			<li><a href="#slider2" class="black-text"><i class="fa fa-info-circle"></i>En Mora</a></li>
+			<li><a href="#slider2" class="black-text"><i class="fa fa-heartbeat"></i>Citas Medicas</a></li>
+			<li><a href="#slider2" class="black-text"><i class="fa fa-sign-out"></i>Salir</a></li>
+		</ul>
+	</div>
+</nav>
