de7fa9781d759ab7e3daed0ac137af0adb1c3203
jherson <jjvasco9@misena.edu.co>
2016-06-07 05:42:46 -0500

    Base de datos 07/06/16

--- WEBAPP/Model/crossfitlife-DB.sql
+++ WEBAPP/Model/crossfitlife-DB.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.4.14
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-05-2016 a las 00:33:08
--- Versión del servidor: 10.1.9-MariaDB
--- Versión de PHP: 5.5.30
+-- Tiempo de generación: 07-06-2016 a las 12:41:14
+-- Versión del servidor: 5.6.26
+-- Versión de PHP: 5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `actividad`
 --
 
-CREATE TABLE `actividad` (
+CREATE TABLE IF NOT EXISTS `actividad` (
   `act_cod` varchar(10) NOT NULL,
   `rec_cod` varchar(10) NOT NULL,
   `act_duracion` varchar(10) DEFAULT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE `actividad` (
 -- Estructura de tabla para la tabla `actividad_x_rutina`
 --
 
-CREATE TABLE `actividad_x_rutina` (
+CREATE TABLE IF NOT EXISTS `actividad_x_rutina` (
   `rut_cod` varchar(10) NOT NULL,
   `act_cod` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -52,13 +52,20 @@ CREATE TABLE `actividad_x_rutina` (
 -- Estructura de tabla para la tabla `cita`
 --
 
-CREATE TABLE `cita` (
-  `cita_cod` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `cita` (
+  `cita_cod` varchar(15) NOT NULL,
   `usu_cod` varchar(10) NOT NULL,
-  `nom_med` varchar(50) NOT NULL,
-  `cita_fecha` varchar(10) NOT NULL,
+  `nom_med` varchar(50) DEFAULT NULL,
+  `cita_fecha` varchar(15) NOT NULL,
   `cita_hora` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `cita`
+--
+
+INSERT INTO `cita` (`cita_cod`, `usu_cod`, `nom_med`, `cita_fecha`, `cita_hora`) VALUES
+('57163142547', '1023', NULL, '9 June, 2016', '13:00');
 
 -- --------------------------------------------------------
 
@@ -66,7 +73,7 @@ CREATE TABLE `cita` (
 -- Estructura de tabla para la tabla `permiso`
 --
 
-CREATE TABLE `permiso` (
+CREATE TABLE IF NOT EXISTS `permiso` (
   `perm_cod` varchar(10) NOT NULL,
   `perm_nom` varchar(20) NOT NULL,
   `perm_fecha` varchar(10) NOT NULL
@@ -78,7 +85,7 @@ CREATE TABLE `permiso` (
 -- Estructura de tabla para la tabla `permiso_x_rol`
 --
 
-CREATE TABLE `permiso_x_rol` (
+CREATE TABLE IF NOT EXISTS `permiso_x_rol` (
   `rol_cod` varchar(10) NOT NULL,
   `perm_cod` varchar(10) NOT NULL,
   `estado` varchar(20) NOT NULL,
@@ -92,7 +99,7 @@ CREATE TABLE `permiso_x_rol` (
 -- Estructura de tabla para la tabla `plan`
 --
 
-CREATE TABLE `plan` (
+CREATE TABLE IF NOT EXISTS `plan` (
   `plan_cod` varchar(10) NOT NULL,
   `factura_cod` varchar(20) NOT NULL,
   `plan_precio` varchar(10) NOT NULL,
@@ -101,13 +108,22 @@ CREATE TABLE `plan` (
   `plan_rango` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `plan`
+--
+
+INSERT INTO `plan` (`plan_cod`, `factura_cod`, `plan_precio`, `plan_desc`, `plan_dias`, `plan_rango`) VALUES
+('1', '321654', '50000', 'Familiar 1 mes', '30', '30'),
+('2', '456785', '36550', 'un mes', '30', '30'),
+('3', '50457', '50000', '2 meses', '60', '60');
+
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `recurso_fisico`
 --
 
-CREATE TABLE `recurso_fisico` (
+CREATE TABLE IF NOT EXISTS `recurso_fisico` (
   `rec_cod` varchar(10) NOT NULL,
   `rec_nom` varchar(50) NOT NULL,
   `rec_img` varchar(255) DEFAULT NULL,
@@ -121,7 +137,7 @@ CREATE TABLE `recurso_fisico` (
 -- Estructura de tabla para la tabla `rol`
 --
 
-CREATE TABLE `rol` (
+CREATE TABLE IF NOT EXISTS `rol` (
   `rol_cod` varchar(10) NOT NULL,
   `rol_nom` varchar(15) NOT NULL,
   `rol_fecha` varchar(10) NOT NULL
@@ -132,8 +148,8 @@ CREATE TABLE `rol` (
 --
 
 INSERT INTO `rol` (`rol_cod`, `rol_nom`, `rol_fecha`) VALUES
-('1', 'Administrador', '26/05/2016'),
-('4', 'Cliente', '26/05/2016');
+('1', 'Administrador', '29/05/2016'),
+('4', 'Cliente', '29-05-2016');
 
 -- --------------------------------------------------------
 
@@ -141,7 +157,7 @@ INSERT INTO `rol` (`rol_cod`, `rol_nom`, `rol_fecha`) VALUES
 -- Estructura de tabla para la tabla `rutina`
 --
 
-CREATE TABLE `rutina` (
+CREATE TABLE IF NOT EXISTS `rutina` (
   `rut_cod` varchar(10) NOT NULL,
   `vlrcion_cod` varchar(10) NOT NULL,
   `rut_nom` varchar(30) NOT NULL,
@@ -155,7 +171,7 @@ CREATE TABLE `rutina` (
 -- Estructura de tabla para la tabla `seguimiento`
 --
 
-CREATE TABLE `seguimiento` (
+CREATE TABLE IF NOT EXISTS `seguimiento` (
   `segui_cod` varchar(10) NOT NULL,
   `usu_cod` varchar(15) NOT NULL,
   `segui_modu` varchar(50) NOT NULL,
@@ -170,7 +186,7 @@ CREATE TABLE `seguimiento` (
 -- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `usuario` (
+CREATE TABLE IF NOT EXISTS `usuario` (
   `usu_cod` varchar(15) NOT NULL,
   `plan_cod` varchar(10) DEFAULT NULL,
   `rol_cod` varchar(10) NOT NULL,
@@ -182,19 +198,46 @@ CREATE TABLE `usuario` (
   `usu_mail` varchar(40) DEFAULT NULL,
   `usu_dir` varchar(30) DEFAULT NULL,
   `usu_estado` varchar(10) NOT NULL,
-  `usu_edad` int(11) NOT NULL,
+  `usu_edad` int(11) DEFAULT NULL,
   `usu_fecha` date NOT NULL,
-  `usu_inicio_plan` varchar(10) DEFAULT NULL,
-  `usu_fin_plan` varchar(10) DEFAULT NULL
+  `usu_plan_inicio` varchar(10) DEFAULT NULL,
+  `usu_plan_fin` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`usu_cod`, `plan_cod`, `rol_cod`, `usu_clave`, `usu_nom`, `usu_ape`, `usu_tel`, `usu_cel`, `usu_mail`, `usu_dir`, `usu_estado`, `usu_edad`, `usu_fecha`, `usu_inicio_plan`, `usu_fin_plan`) VALUES
-('1023', NULL, '1', 'Admin', 'Administrador', 'Todos', NULL, NULL, NULL, NULL, 'Activo', 0, '2016-05-26', NULL, NULL),
-('457784641ss', NULL, '4', 'dfsdfsds', 'mauricio', 'calle', '3622204', '3216318706', 'sdfsdf', 'asdasd', 'Activo', 20, '2016-05-27', '', '');
+INSERT INTO `usuario` (`usu_cod`, `plan_cod`, `rol_cod`, `usu_clave`, `usu_nom`, `usu_ape`, `usu_tel`, `usu_cel`, `usu_mail`, `usu_dir`, `usu_estado`, `usu_edad`, `usu_fecha`, `usu_plan_inicio`, `usu_plan_fin`) VALUES
+('1023', NULL, '1', '123456', 'jherson', 'vasco', NULL, NULL, NULL, NULL, 'Activo', NULL, '2016-05-29', NULL, NULL),
+('3658', NULL, '4', '123456', 'juan', 'pelaez', '', '', '', '', 'Activo', 30, '2016-06-04', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_casual`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario_casual` (
+  `usu_cas_cod` varchar(10) NOT NULL,
+  `usu_cas_nom` varchar(50) NOT NULL,
+  `usu_cas_ape` varchar(50) NOT NULL,
+  `usu_cas_tel` varchar(15) NOT NULL,
+  `usu_cas_fecha` date NOT NULL,
+  `usu_cas_hora` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario_casual`
+--
+
+INSERT INTO `usuario_casual` (`usu_cas_cod`, `usu_cas_nom`, `usu_cas_ape`, `usu_cas_tel`, `usu_cas_fecha`, `usu_cas_hora`) VALUES
+('1036660429', 'jherson', 'vasco', '3622204', '2016-06-01', '00:00:00'),
+('1036660429', 'jherson', 'vasco', '3622204', '2016-06-04', '01:57:21'),
+('1036660429', 'jherson', 'vasco', '3622204', '2016-06-03', '19:04:34'),
+('1036660429', 'jherson', 'vasco', '3622204', '2016-06-03', '19:05:48'),
+('1036660429', 'jherson', 'vasco', '3622204', '2016-06-03', '19:06:25'),
+('1036660429', 'jherson', 'vasco', '3622204', '2016-06-03', '19:06:33');
 
 -- --------------------------------------------------------
 
@@ -202,7 +245,7 @@ INSERT INTO `usuario` (`usu_cod`, `plan_cod`, `rol_cod`, `usu_clave`, `usu_nom`,
 -- Estructura de tabla para la tabla `valoracion`
 --
 
-CREATE TABLE `valoracion` (
+CREATE TABLE IF NOT EXISTS `valoracion` (
   `vlrcion_cod` varchar(10) NOT NULL,
   `usu_cod` varchar(15) NOT NULL,
   `vlrcion_peso` int(11) NOT NULL,
