3d9ee9da457c515d1521f5fd4d6ab9e3084f2855
omky <omcalle2@misena.edu.co>
2016-08-01 00:38:42 -0500

    formverplan

--- Website/html/contenido/VerPlan.php
+++ Website/html/contenido/VerPlan.php
@@ -11,9 +11,18 @@
  						<H4>Datos del Plan</H4>
  					</head>
  					<tbody>
- 						<tr class= "colm12 s12"
-
+ 						<tr class= "colm12 s12">
+ 							<td class="colm12 s12">
+ 							<label><h5>Código del Plan: <?php echo $verplanes["plan_cod"]; ?></h5></label>
+ 							<input type="hidden" name="codplanver" value="<?php echo $verplanes["plan_cod"] ?>;" onKeypress="if(event.keyCode < 45 || (event.keyCode > 57)) event.returnValue = false;">
+ 							</td>
  						</tr>
- 					</tbody>>
+ 						<tr class= "colm12 s12">
+ 							<td class="col m3 s3">
+ 								<label for="nombres">Código de la Factura</label>
+ 								<input type="text" name="facturaplanver" id="facturaplanver" value="<?php echo $verplanes["factura_cod"]; ?>">
+ 							</td>
+ 						
+ 					</tbody>
  				</table>
 
