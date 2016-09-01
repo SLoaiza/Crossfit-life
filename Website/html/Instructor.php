cd977a0f83047b42ea1abbf95afc072a84ca985a
SLoaiza <sloaiza44@misena.edu.co>
2016-09-01 17:18:55 -0500

    yoquec

---
+++ Website/html/Instructor.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>instructor</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css">
+
+    <link rel="stylesheet" href="../css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,100italic,700,900,900italic,700italic,500italic,500,400italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Share+Tech+Mono' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../WEBAPP/Views/Assets/Plugins/jquery.ptTimeSelect.css">
+    <link rel="stylesheet" href="../../WEBAPP/Views/Assets/Plugins/sweetalert.css">
+  </head>
+  <body>
+    <?php
+      include('menus/menuInst.php')
+
+     ?>
+  </body>
+</html>
---
+++ Website/html/menus/menuInst.php
@@ -0,0 +1,39 @@
+<nav class="z-depth-2 m12 s12 menu" >
+	<div class="nav-wrapper  z-depth-1" color:"#424242">
+		<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons fa fa-bars"></i></a>
+		<a href="#!" class="dropdown-button black-text right" data-activates="dropdown2" style="margin-right: 20px;"><i class="fa fa-cog fa-3x"></i></a>
+		<ul class="hide-on-med-and-down">
+			<li><a href="instructor.php?S=<?php echo base64_encode("perfil");?>" class="whithe-text">Inicio</a></li>
+			<li><a href="instructor.php?S=<?php echo base64_encode("actividad");?>" class="whithe-text">Gestionar actividades</a></li>
+      <li><a href="instructor.php?S=<?php echo base64_encode("rutina");?>" class="whithe-text">Gestionar rutinas</a></li>
+			<li><a href="instructor.php?S=<?php echo base64_encode("valoraciones");?>" class="whithe-text">Ver valoraciones</a></li>
+			<!-- <li><a class="dropdown-button white-text" href="#!" data-activates="dropdown1"><i class="fa fa-caret-down"></i>&nbsp;&nbsp;Gestiones</a></li> -->
+		</ul>
+		<ul class="side-nav" id="mobile-demo">
+			<li><a href="#cabeza" class="black-text"><i class="fa fa-home"></i>Inicio</a></li>
+			<li><a href=".slider" class="black-text"><i class="fa fa-sign-in"></i>Ingreso De Usuarios</a></li>
+			<li><a href="#quienes_somos" class="black-text"><i class="fa fa-user-plus"></i>Nuevo Cliente</a></li>
+			<li><a href="#slider2" class="black-text"><i class="fa fa-heartbeat"></i>Citas Medicas</a></li>
+			<li><a href="#slider2" class="black-text">Citas Medicas</a></li>
+			<li><a href="#slider2" class="black-text">Valoraciones</a></li>
+			<li><a href="#slider2" class="black-text">Roles</a></li>
+			<li><a href="#slider2" class="black-text">Planes</a></li>
+			<li><a href="#slider2" class="black-text">Recursos Fisicos</a></li>
+			<li><a href="index.php" class="black-text"><i class="fa fa-sign-out"></i>Salir</a></li>
+		</ul>
+
+
+		<ul id="dropdown2" class="dropdown-content">
+			<li><a href=""><i class="fa fa-user fa-1x"> Usuario</i></a></li>
+			<li><a href="contenido/cerrarsesion.php"><i class="fa fa-power-off fa-1x"> Salir</i></a></li>
+			<li><a href=""></a></li>
+		</ul>
+		<!-- <ul id="dropdown1" class="dropdown-content"  >
+		  	<li><a href="Admin.php?S=<?php //echo base64_encode("citas");?>" class="black-text">Citas Medicas</a></li>
+			<li><a href="Admin.php?S=<?php //echo base64_encode("valoracion");?>" class="black-text">Valoraciones</a></li>
+			<li><a href="Admin.php?S=<?php //echo base64_encode("planes");?>" class="black-text">Planes</a></li>
+			<li><a href="Admin.php?S=<?php //echo base64_encode("regirecur");?>" class="black-text">Recursos Fisicos</a></li>
+			<li><a href="Admin.php?S=<?php //echo base64_encode("AllUsers");?>" class="black-text">Ver Todos Los Clientes</a></li>
+		</ul> -->
+	</div>
+</nav>
